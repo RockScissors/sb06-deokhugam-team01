53f879c8f6ac84a2af9e44d32cac3a563396de15
taehun88 <taehun8235@gmail.com>
2025-11-24 16:44:02 +0900

    fix: remove exception

--- src/main/java/com/sprint/sb06deokhugamteam01/exception/common/InvalidException.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/exception/common/InvalidException.java
@@ -4,11 +4,10 @@ import com.sprint.sb06deokhugamteam01.exception.ErrorCode;
 import com.sprint.sb06deokhugamteam01.exception.RootException;
 import java.util.Map;
 
-public class InvalidRequestException extends RootException {
+public class InvalidException extends RootException {
 
     ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
-
-    public InvalidRequestException(Map<String, Object> details) {
-        super(ErrorCode.INVALID_REQUEST, details);
+    public InvalidException(Map<String, Object> detail) {
+        super(ErrorCode.INVALID_REQUEST, detail);
     }
 }
