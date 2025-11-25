36adac57b632c88e0fdedf63da2f3fdf53208b2c
김재원 <dumdum7@hs.ac.kr>
2025-11-25 15:45:41 +0900

    feat: 커스텀 예외 추가

--- src/main/java/com/sprint/sb06deokhugamteam01/exception/book/AlReadyExistsIsbnException.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/exception/book/AlReadyExistsIsbnException.java
@@ -1,9 +1,15 @@
 package com.sprint.sb06deokhugamteam01.exception.book;
 
+import com.sprint.sb06deokhugamteam01.exception.ErrorCode;
 import com.sprint.sb06deokhugamteam01.exception.RootException;
 
+import java.util.Map;
+
 public class AlReadyExistsIsbnException extends RootException {
-    public AlReadyExistsIsbnException(String message) {
-        super(message);
+
+    ErrorCode errorCode = ErrorCode.ALREADY_EXISTS_ISBN;
+
+    public AlReadyExistsIsbnException(Map<String, Object> details) {
+        super(ErrorCode.ALREADY_EXISTS_ISBN, details);
     }
 }
